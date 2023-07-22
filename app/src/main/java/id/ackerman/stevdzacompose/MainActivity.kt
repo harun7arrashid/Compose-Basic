648b4fc178a77f469f87ff7c3fb37f0af627cb78
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-07-22 14:42:37 +0700

    TextField

--- app/src/main/java/id/ackerman/stevdzacompose/MainActivity.kt
+++ app/src/main/java/id/ackerman/stevdzacompose/MainActivity.kt
@@ -1,24 +1,30 @@
 package id.ackerman.stevdzacompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,47 +43,75 @@ class MainActivity : ComponentActivity() {
         setContent {
             StevdzaComposeTheme {
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.primary)
-                        .padding(24.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-
-                ) {
-
-                    ExpandableCard(
-                        title = "Modern Android Development",
-                        desc = "Lorem ipsu asdkjh asd asidh asidh asnd asidh asd lkasdj easdiha llasdlasdl fjasdladlasd nd aslda lasdlalsdl askd lemnlaksdhjiolashdioasdhioashdoasihdoaidhaioh laksnd lknweq lawskj a sdlkhjasd lkn lkansdlk alskdnalksdnalksdh lkahsd lakshj dalkhsd laksdh woqiey qwioey adl nasld khlaksdhjalsidku oiasduoasd oasidhaoi sdhoaishdoiqwheoqiwheoqiweh oiqwehoiqwehoiqwehioqwehoqwiheqwioeh ioqweh"
-                    )
-
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting()
                 }
+
             }
         }
+    }
+}
+
+@Composable
+fun Greeting() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "This is Title") },
+            singleLine = true,
+            placeholder = { Text(text = "This is Hint") },
+            leadingIcon = {
+
+                IconButton(onClick = {  }) {
+                    Icon(
+                        imageVector = Icons.Filled.Email,
+                        contentDescription = "Email Icon"
+                    )
+                }
+            },
+
+            trailingIcon = {
+
+                IconButton(onClick = {  }) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Cheklis Icon"
+                    )
+                }
+            },
+
+            keyboardOptions  = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction    = ImeAction.Search
+            ),
+
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("Harun", "Clicked onSearch")
+                }
+            )
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    StevdzaComposeTheme() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.primary)
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    StevdzaComposeTheme {
 
-        ) {
+        Greeting()
 
-            ExpandableCard(
-                title = "Modern Android Development",
-                desc = "Lorem ipsu asdkjh asd asidh asidh asnd asidh asd lkasdj easdiha llasdlasdl fjasdladlasd nd aslda lasdlalsdl askd lemnlaksdhjiolashdioasdhioashdoasihdoaidhaioh laksnd lknweq lawskj a sdlkhjasd lkn lkansdlk alskdnalksdnalksdh lkahsd lakshj dalkhsd laksdh woqiey qwioey adl nasld khlaksdhjalsidku oiasduoasd oasidhaoi sdhoaishdoiqwheoqiwheoqiweh oiqwehoiqwehoiqwehioqwehoqwiheqwioeh ioqweh"
-            )
-
-        }
     }
 }
 
